a7760950537bfff3dc2ed065cc23606fc5b92843
11pramod <gvpramodreddy1109@outlook.com>
2023-08-29 22:40:58 +0530

    Terraform files provider updated

--- private_routes.tf
+++ private_routes.tf
@@ -1,4 +1,4 @@
-resource "aws_route" "private_subnet" {
+resource "aws_route_table" "private_subnet" {
   vpc_id = aws_vpc.vpc1.id
   tags = {
     name        = "${var.vpc}-private RT"
@@ -10,5 +10,5 @@ resource "aws_route" "private_subnet" {
 resource "aws_route_table_association" "private-RT-ass" {
   count          = length(local.subnet_private_cidr)
   subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = aws_route.private_subnet.id
+  route_table_id = aws_route_table.private_subnet.id
 }
