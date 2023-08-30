05d3fd859c637d049703ac99d83b4b0668e535ae
11pramod <gvpramodreddy1109@outlook.com>
2023-08-30 14:24:51 +0530

    chnages

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "ec2" {
   ami                    = "ami-0261755bbcb8c4a84"
   key_name               = "ansible"
   subnet_id              = element(aws_subnet.public.*.id, count.index)
+  associate_public_ip_address = true
   vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
   tags = {
     name              = "${var.vpc}-PublicServer-${count.index + 1}"
